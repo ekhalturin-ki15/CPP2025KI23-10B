6983f94cbdc00fb1a6d9eaeb7b04e726489f1301
DORANDKOR <loslt@duck.com>
2025-09-18 15:48:50 +0700

    generate characters

--- Character_Controller.cpp
+++ Character_Controller.cpp
@@ -3,13 +3,30 @@
 
 void Character_Controller::init(ifstream& in)
 {
+    int number_of_characters;
+    
+    
+    
     Player = make_shared<Character>();
     in >> Player->hp;
     in >> Player->damage;
     in >> Player->armor;
     in >> Player->water;
-    in >> Player->eat;
+    in >> Player->food;
     in >> Player->name;
+    
+    in >> number_of_characters;
+    arr_Character.resize(number_of_characters);
+    for (auto& element : arr_Character)
+    {
+        element = make_shared<Character>();
+        in >> element->hp;
+        in >> element->damage;
+        in >> element->armor;
+        in >> element->water;
+        in >> element->food;
+        in >> element->name;
+    }
     /*string name1;
     in >> name1;
     Player -> name = new string(name1);
