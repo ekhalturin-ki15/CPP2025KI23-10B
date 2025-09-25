c9ee1db7bb28c535ee1ebf6e802461ea8981085b
Khalturin Evgenii <1tigrin@mail.ru>
2025-09-25 11:26:14 +0300

    GameMode

---
+++ Game_Mode.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <memory>
+#include <fstream>
+#include <iostream>
+
+class Character_Controller;
+class Game_Loop;
+
+using namespace std;
+
+class Game_Mode
+{
+public:
+    Game_Mode();
+    std::ifstream in;
+    std::ofstream out;
+
+    // TSubclassOf
+	shared_ptr<Character_Controller> CC; // Отслеживать хранение в памяти
+	//Character_Controller* ptrCC; // Передавать данны, которые хранятися
+
+    shared_ptr<Game_Loop>            GL;
+    shared_ptr<Game_Mode>            This;
+
+    //Game_Loop*                     ptrGL;
+
+};
--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
-﻿#include "Character.h"
-#include "Pet.h"
-#include <fstream>
-#include <iostream>
-#include <memory>
-#include "Character_Controller.h"
-
+﻿#include "Game_Mode.h"
 int main()
 {
-    //
-    std::ifstream in;
-    std::ofstream out;
-
-    in.open("input.txt");
-    out.open("output.txt");
-    {
-        Character_Controller FCharacterController;
-        FCharacterController.init(in);
-    }
-
+    shared_ptr<Game_Mode> GM;
 
 }
